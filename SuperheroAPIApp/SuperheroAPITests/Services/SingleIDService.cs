508001172e9839c6ab54d47599e211b9365335c9
sabirnisar <56258927+sabirnisar@users.noreply.github.com>
2021-08-20 14:51:12 +0100

    Update SingleIDService.cs

--- SuperheroAPIApp/SuperheroAPITests/Services/SingleIDService.cs
+++ SuperheroAPIApp/SuperheroAPITests/Services/SingleIDService.cs
@@ -8,5 +8,29 @@ namespace SuperheroAPITests.Services
 {
     public class SingleIDService
     {
+<<<<<<< Updated upstream
+=======
+        #region Properties
+
+        public CallManager CallManager { get; set; }
+        public JObject Json_response { get; set; }
+        public DTO<ID> SingleIdDTO { get; set; }
+        public string IdSelected { get; set; }
+        public string IdResponse { get; set; }
+        #endregion
+        public SingleIDService()
+        {
+            CallManager = new CallManager();
+            SingleIdDTO = new DTO<ID>();
+        }
+
+        public async Task MakeRequestAsync(int id)
+        {
+            IdResponse = await CallManager.MakeIdRequestAsync(id);
+            Json_response = JObject.Parse(IdResponse);
+            SingleIdDTO.DeserializeResponse(IdResponse);
+
+        }
+>>>>>>> Stashed changes
     }
 }
