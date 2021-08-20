3e9fd42630835aeda99e2fdd68219ef0078f60f8
atchu98 <atchuthan98@googlemail.com>
2021-08-20 15:21:29 +0100

    First Name Test

--- SuperheroAPIApp/SuperheroAPITests/Tests/NameTests.cs
+++ SuperheroAPIApp/SuperheroAPITests/Tests/NameTests.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using SuperheroAPITests.Services;
 
 namespace SuperheroAPITests.Tests
 {
     public class NameTests
     {
+          NameService  _nameService;
+
+        public async Task OneTimeSetupAsync()
+        {
+            _nameService = new NameService();
+            await _nameService.MakeRequestAsync("Batman");
+        }
+
+        [Test]
+        public void StatusIs200()
+        {
+            Assert.That(_nameService.CallManager.Status, Is.EqualTo(200));
+        }
+
     }
 }
