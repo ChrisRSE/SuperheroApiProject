d530c57c087d6179fa3356fcefac7f73ecca1ab2
sabirnisar <56258927+sabirnisar@users.noreply.github.com>
2021-08-20 12:50:36 +0100

    Completed Model for ID and Name, deleted Bulk classes

--- SuperheroAPIApp/SuperheroAPITests/DataHandling/DTO.cs
+++ SuperheroAPIApp/SuperheroAPITests/DataHandling/DTO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SuperheroAPITests.Services
+namespace SuperheroAPITests.DataHandling
 {
-    class BulkIDService
+    public class DTO
     {
     }
 }
--- SuperheroAPIApp/SuperheroAPITests/Tests/SingleIDTests.cs
+++ SuperheroAPIApp/SuperheroAPITests/Tests/SingleIDTests.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace SuperheroAPITests.Tests
 {
-    class SingleIDTests
+    public class SingleIDTests
     {
     }
 }
