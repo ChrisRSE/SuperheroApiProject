2a6b9d316b95b9419bd0a0cc805762cea8dcfcb5
sabirnisar <56258927+sabirnisar@users.noreply.github.com>
2021-08-20 14:46:03 +0100

    Updated CallManager

--- SuperheroAPIApp/SuperheroAPITests/HTTP/CallManager.cs
+++ SuperheroAPIApp/SuperheroAPITests/HTTP/CallManager.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json.Linq;
 
 namespace SuperheroAPITests.HTTP
 {
     public class CallManager
     {
+        private readonly IRestClient _client;
+        public int Status { get; set; }
+        public CallManager()
+        {
+            _client = new RestClient(AppConfigReader.BaseUrl);
+        }
+        public async Task<string> MakeIdRequestAsync(int Id)
+        {
+            var request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            request.Resource = $"{Id}";
+            var response = await _client.ExecuteAsync(request);
+            Status = (int)response.StatusCode;
+            return response.Content;
+        }
     }
 }
