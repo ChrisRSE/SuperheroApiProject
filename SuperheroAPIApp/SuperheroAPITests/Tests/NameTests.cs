4f50cbd9a14b03f06b902e3b015420e76c45b5b3
atchu98 <atchuthan98@googlemail.com>
2021-08-20 16:11:40 +0100

    More Name Tests

--- SuperheroAPIApp/SuperheroAPITests/Tests/NameTests.cs
+++ SuperheroAPIApp/SuperheroAPITests/Tests/NameTests.cs
@@ -10,20 +10,45 @@ namespace SuperheroAPITests.Tests
 {
     public class NameTests
     {
-          NameService  _nameService;
-
+        NameService  _nameGoodService;
+        NameService _nameBadService;
+        [OneTimeSetUp]
         public async Task OneTimeSetupAsync()
         {
-            _nameService = new NameService();
-            await _nameService.MakeRequestAsync("Batman");
+            _nameGoodService = new NameService();
+            await _nameGoodService.MakeRequestAsync("Batman");
+            _nameBadService = new NameService();
+            await _nameBadService.MakeRequestAsync("-0fgh");
         }
 
         [Test]
-        public void StatusIs200()
+        [Category ("Happy Path")]
+        public void StatusIs200WhenValidSuperheroNameIsUsed()
         {
-            Assert.That(_nameService.CallManager.Status, Is.EqualTo(200));
+            Assert.That(_nameGoodService.CallManager.Status, Is.EqualTo(200));
         }
 
+        [Test]
+        [Category("Happy Path")]
+        public void CheckDetailsCorrectWithValidName()
+        {
+            Assert.That(_nameGoodService.CountNumberOfCharatcterVersions(), Is.EqualTo(3));
+        }
+
+        [Test]
+        [Category("Sad Path")]
+        public void StatusIsDefault0_WhenNoValueIsPassed()
+        {
+            var testNameService = new NameService();
+            Assert.That(testNameService.CallManager.Status, Is.EqualTo(0));
+        }
+
+        [Test]
+        [Category("Sad Path")]
+        public void StatusIs400WhenInvalidSuperheroNameIsUsed()
+        {
+            Assert.That(_nameBadService.Json_Response["response"].ToString(), Is.EqualTo("error"));
+        }
 
 
     }
